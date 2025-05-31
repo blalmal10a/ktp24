21ca96e9e3906ec03f657f6742b871f5b4e7d89e
blalmal10a <malsawma7777@gmail.com>
2025-05-31 11:48:08 +0530

    fix: search UI bug, cannot scroll

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -3,16 +3,35 @@
   <div
     v-else
     class="full-width"
+    style="height: 100%;"
   >
-    <div style="position: sticky; top:0;">
+    <div style="position: sticky; top:0; backdrop-filter: blur(10px); z-index: 99;">
       <q-input
         autofocus
         @update:model-value="searchFilter"
         placeholder="Search song"
         type="text"
-        label="Search song"
         class="q-px-md"
-      />
+      >
+        <template v-slot:prepend>
+          <q-icon
+            size="sm"
+            name="search"
+          />
+        </template>
+        <template v-slot:append>
+          <q-icon
+            name="close"
+            @click="() => {
+              searchFilter('');
+              showSearch = false;
+              if (songIndex == null) {
+                songIndex = 0;
+              }
+            }"
+          />
+        </template>
+      </q-input>
     </div>
     <template
       v-for="(item, index) in filteredSongs"
